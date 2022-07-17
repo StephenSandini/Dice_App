891a12b1e37f81d146d7f38df99c5996c9687ccf
StephenSandini <96503276+StephenSandini@users.noreply.github.com>
2022-07-17 15:43:01 -0500

    l1p1_completed for stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:dice_template_app/viewscreen/imagedemo_screen.dart';
+import 'package:dice_template_app/viewscreen/materialdesign_screen.dart';
 import 'package:dice_template_app/viewscreen/start_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'model/constant.dart';
 
 void main() {
   runApp(const Dice_App());
@@ -12,17 +16,30 @@ class Dice_App extends StatelessWidget {
   Widget build(BuildContext context) {
     //For the Routing of MaterialApp
     return MaterialApp(
+      //Displays or Removes the Debug Banner
+      debugShowCheckedModeBanner: Constant.devMode,
+      theme: ThemeData(
+        //Changes between dark and light mode
+        brightness: Brightness.dark,
+        //Overrides text theme, in this case we are doing headline 1
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontSize: 64.0,
+            color: Colors.blueGrey,
+          ),
+          button: TextStyle( fontSize: 28.0,),
+        ),
+      ),
       //Initial Route when App Starts
       initialRoute: StartScreen.routeName,
       //Routing has a key value pair
       //Screen.name of route : function
       routes: {
         StartScreen.routeName: (BuildContext context) => const StartScreen(),
+        MaterialDesignClass.routeName: (BuildContext context) =>
+            const MaterialDesignClass(),
+        ImageDemoScreen.routeName: (BuildContext context) => const ImageDemoScreen(),
       },
     );
   }
-}
-
-Widget f1(BuildContext context) {
-  return const StartScreen();
 }
--- lib/viewscreen/start_screen.dart
+++ lib/viewscreen/start_screen.dart
@@ -1,3 +1,5 @@
+import 'package:dice_template_app/viewscreen/imagedemo_screen.dart';
+import 'package:dice_template_app/viewscreen/materialdesign_screen.dart';
 import 'package:dice_template_app/viewscreen/view/view_util.dart';
 import 'package:flutter/material.dart';
 
@@ -28,18 +30,120 @@ class StartScreen extends StatelessWidget {
             ),
             icon: const Icon(Icons.message),
           ),
+          PopupMenuButton(
+            onSelected: (String value) {
+              showSnackBar(context: context, message: 'Popupmenu = $value');
+            },
+            itemBuilder: (BuildContext context) {
+              return <PopupMenuItem<String>>[
+                 PopupMenuItem(
+                  value: 'License',
+                  child: Row(
+                    children: const [
+                      Icon(Icons.description_outlined),
+                      Text('License'),
+                    ],
+                  ),
+                ),
+                 PopupMenuItem(
+                  value: 'Balance',
+                  child: Row(
+                    children: const [
+                      Icon(Icons.account_balance),
+                      Text('Balance'),
+                    ],
+                  ),
+                ),
+                 PopupMenuItem(
+                  value: 'Profile',
+                  child: Row(
+                    children: const [
+                      Icon(Icons.person),
+                      Text('Profile'),
+                    ],
+                  ),
+                ),
+              ];
+            },
+          ),
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const UserAccountsDrawerHeader(
+              currentAccountPicture:
+                  Icon(Icons.account_circle_rounded, size: 80.0),
+              accountName: Text('Placed Holder Account Name'),
+              accountEmail: Text('Place Holder Account Email'),
+            ),
+            ListTile(
+              leading: const Icon(Icons.people),
+              title: const Text('Friends'),
+              onTap: () {
+                Navigator.of(context).pop(); //Close the Drawer
+                showSnackBar(context: context, message: 'Friends Menu');
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.exit_to_app),
+              title: const Text('Logout'),
+              onTap: () {
+                Navigator.of(context).pop(); //Close the Drawer
+                showSnackBar(context: context, message: 'Logout Menu');
+              },
+            ),
+          ],
+        ),
       ),
       body: Column(
         children: [
-          const Text('Choose a menu to navigate'),
-          ElevatedButton(
-            onPressed: () => showSnackBar(context: context, message: 'Button 1'),
-            child: const Text('Button 1'),
+          Text(
+            'Choose a menu to navigate',
+            style: Theme.of(context).textTheme.headline5,
           ),
-           ElevatedButton(
-            onPressed: () => showSnackBar(context: context, message: 'Button 2'),
-            child: const Text('Button 2'),
+          ElevatedButton(
+            onPressed: () =>
+                Navigator.pushNamed(context, MaterialDesignClass.routeName),
+            child: Text(
+              'Material Design Style',
+              style: Theme.of(context).textTheme.button,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () => Navigator.pushNamed(context, ImageDemoScreen.routeName),
+            child: Text(
+              'Image Demo',
+              style: Theme.of(context).textTheme.button,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showSnackBar(context: context, message: 'Floating Action Button');
+        },
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        //currentIndex: , specifies which tab is selected
+        onTap: (int index) {
+          showSnackBar(
+              context: context,
+              message: 'Bottom Navigation Bar: index = $index');
+        },
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Time',
+            icon: Icon(Icons.time_to_leave),
+          ),
+          BottomNavigationBarItem(
+            label: 'Business',
+            icon: Icon(Icons.business),
           ),
         ],
       ),
